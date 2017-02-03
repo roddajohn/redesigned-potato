a42397dc6316dc0fd80ded0584b1afd085a81995
JiaqiGao <jiaqigao12@gmail.com>
2017-02-03 14:56:58 -0500

    db_builder.py

--- db_builder.py
+++ db_builder.py
@@ -1,1 +1,32 @@
 from pymongo import MongoClient
+
+server = MongoClient("lisa@stuy.edu")
+
+db = server["redesigned-potato"]
+
+Cdicts = []
+lines = open("courses.csv").read().split('\n')[1:]
+
+for line in lines:
+    if line == "":
+        continue;
+    newdict = {}
+    newdict['code'] = line.split(',')[0]
+    newdict['mark'] = line.split(',')[1]
+    newdict['id'] = line.split(',')[2]
+    dicts.append(newdict)
+
+db.courses.insert_many(dicts)
+
+Pdicts = []
+lines = open("courses.csv").read().split('\n')[1:]
+
+for line in lines:
+    if line == "":
+        continue;
+    newdict = {}
+    newdict['code'] = line.split(',')[0]
+    newdict['mark'] = line.split(',')[1]
+    newdict['id'] = line.split(',')[2]
+    dicts.append(newdict)
+
